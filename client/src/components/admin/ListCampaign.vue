03bf5aa261c77a9042ca9ba5857778e22c4f5608
TRP <akemonai@gmail.com>
2020-12-30 17:23:48 +0700

    config route

--- client/src/components/admin/ListCampaign.vue
+++ client/src/components/admin/ListCampaign.vue
@@ -27,7 +27,7 @@
                 <td>{{ item.name }}</td>
                 <td>
                   
-                    <v-btn style="margin-left:5%; margin-top:3%; margin-bottom:3%;" class="ma-2"  color="purple" to = "/editaddnews"  dark><v-icon dark>mdi-wrench</v-icon></v-btn>
+                    <v-btn style="margin-left:5%; margin-top:3%; margin-bottom:3%;" class="ma-2"  color="purple" to = "/editnews"  dark><v-icon dark>mdi-wrench</v-icon></v-btn>
                     <v-btn style="margin-left:5%; margin-top:3%; margin-bottom:3%;"><v-icon>{{ icons.mdiDelete }}</v-icon></v-btn>
                   
                 </td>
