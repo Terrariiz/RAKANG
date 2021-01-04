020e8965c2985cad9ab8e2799b58907e8adb97ff
kluaynum1150 <kluaynum951@gmail.com>
2021-01-04 17:36:52 +0700

    แก้หน้าล็อคอินแอดมิน

--- client/src/components/admin/loginAdmin.vue
+++ client/src/components/admin/loginAdmin.vue
@@ -1,41 +1,46 @@
 <template>
   <div class='login'>
-    <h1>This is login page.</h1>
+    <h1>This is admin login page.</h1>
     <v-container>
       <v-form
         ref="form"
         v-model="valid"
         lazy-validation
       >
-        <v-text-field
-          v-model="name"
-          :counter="12"
-          :rules="nameRules"
-          label="Name"
-          required
+        <v-text-field single-line solo
+            id = "username" 
+            v-model="admin.username"  
+            :counter="10" label="Username" 
+            :rules="[rules.required]"  
+            required
         ></v-text-field>
 
-        <v-text-field
-          v-model="password"
-          :counter="12"
-          :rules="passwordRules"
-          label="Password"
-          required
+        <v-text-field single-line solo
+            v-model="admin.password"
+            id="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Password"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
         ></v-text-field>
 
-        <v-text-field
+        <!-- <v-text-field
           v-model="email"
           :rules="emailRules"
           label="E-mail"
           required
-        ></v-text-field>
+        ></v-text-field> -->
 
-        <v-checkbox
+        <!-- <v-checkbox
           v-model="checkbox"
           :rules="[v => !!v || 'You must agree to continue!']"
           label="Do you agree?"
           required
-        ></v-checkbox>
+        ></v-checkbox> -->
 
         <v-btn
           :disabled="!valid"
@@ -61,25 +66,40 @@
 <script>
 export default {
 
-    data: () => ({
-        valid: true,
-        name: '',
-        nameRules: [
-          v => !!v || 'Name is required',
-          v => (v && v.length <= 12) || 'Name must be less than 12 characters',
-        ],
-        password: '',
-        passwordRules: [
-          v => !!v || 'Password is required',
-          v => (v && v.length <= 12) || 'Password must be less than 12 characters',
-        ],
-        email: '',
-        emailRules: [
-          v => !!v || 'E-mail is required',
-          v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-        ],
-        checkbox: false,
-    }),
+    // data: () => ({
+    //     valid: true,
+    //     name: '',
+    //     nameRules: [
+    //       v => !!v || 'Name is required',
+    //       v => (v && v.length <= 12) || 'Name must be less than 12 characters',
+    //     ],
+    //     password: '',
+    //     passwordRules: [
+    //       v => !!v || 'Password is required',
+    //       v => (v && v.length <= 12) || 'Password must be less than 12 characters',
+    //     ],
+    //     email: '',
+    //     emailRules: [
+    //       v => !!v || 'E-mail is required',
+    //       v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    //     ],
+    //     checkbox: false,
+    // }),
+    data(){
+      return{
+        admin:{
+          username:"",
+          password:""
+        },
+        show1:false,
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          emailMatch: () => (`The email and password you entered don't match`),
+        },
+      }
+    },
+    
 
     methods: {
       validate () {
