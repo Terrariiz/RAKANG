d001153738e0a98838dc5f82f7f6e103c4ed2b0e
koladon52 <koladon52@gmail.com>
2021-01-16 23:35:35 +0700

    ความหวังอันแสนริบหรี่

--- client/src/components/admin/payment.vue
+++ client/src/components/admin/payment.vue
@@ -1,9 +1,10 @@
-.<template>
-  <form id="payment-form" action="https://sandbox-cdnv3.chillpay.co/Payment/" method="post" role="form" class="form-horizontal">
+<template>
+
+<form id="payment-form" action="https://sandbox-cdnv3.chillpay.co/Payment/" method="post" role="form" class="form-horizontal">
 <modernpay:widget id="modernpay-widget-container" 
 data-merchantid="M031001" data-amount="10000" data-orderno="00000001" data-customerid="123456" 
-data-mobileno="0889999999" data-clientip="171.96.224.18" data-routeno="1" data-currency="764" 
-data-description="Test Payment" data-apikey="SPYzCOFx0rDm5ZvEwG0O1T8jDZtBqq1H45pw5PUZsHopwn5vc4ThhFwMQrjQVPzz">
+data-mobileno="0889999999" data-clientip="124.120.153.53" data-routeno="1" data-currency="764" 
+data-description="Test Payment" data-apikey="Z5O4ARB0wikPpsSwpjXwmeuVCdD2zVV27Sdbti9gTvYWEOiBo7s7fB6S81LZAE3I">
 </modernpay:widget>
 <button type="submit" id="btnSubmit" value="Submit" class="btn">Payment</button>
 </form>
