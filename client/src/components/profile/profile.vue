e28f5f04c725805b4bed39877b39952d2c191287
flukgaro <pitpagon@hotmail.com>
2021-01-22 14:52:15 +0700

    profile

--- client/src/components/profile/profile.vue
+++ client/src/components/profile/profile.vue
@@ -3,23 +3,18 @@
     <div>
         <Navbar></Navbar>
     </div>
-   <!-- <h1>This is profile page.</h1> -->
-    <v-container>
-        <div class="padding">
-        <div id="borderbox1" class="row container d-flex justify-content-center">
-            <div id="borderbox2">
+    <center><div class="page-content page-container" id="page-content">
+    <div class="padding">
+        <div  class="row container d-flex justify-content-center">
+            <div  class="col-xl-12 col-md-12">
                 <div class="card user-card-full">
-                    <div class="row m-l-0 m-r-0">
-                        <div class="col-sm-4 bg-c-lite-green user-profile">
-                            <div class="card-block text-center text-white">
-
-                                <center><div> <img style="width: 200px; height: 200px; background-size:cover; " :src="'http://localhost:4000/image/profile/' + dataUser.image" class="img-radius base-image-input" alt="User-Profile-Image"> </div></center>
-                                
-                                <hr><h6 style="font-size: 20px; color;blue;" class="f-w-600">{{dataUser.firstname}} {{dataUser.lastname}}</h6>
+                    <div  class="row m-l-0 m-r-0">
+                        <div  class="col-sm-4 bg-c-lite-green user-profile">
+                            <div  class="card-block text-center text-white">
+                                <div class="m-b-25"><center><div> <img style="display: flex;" :src="'http://localhost:4000/image/profile/' + dataUser.image" class="img-radius base-image-input" alt="User-Profile-Image"> </div></center>  </div>
+                                 <hr><h6 style="font-size: 20px; color;blue;" class="f-w-600">{{dataUser.firstname}} {{dataUser.lastname}}</h6>
                                 <h6 class="f-w-600">200 Coin</h6>
-                                
-                                <!-- <p>Web Designer</p> <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i> -->
-                                <v-container fluid>
+                                 <v-container fluid>
                                     <v-row >
                                         <!-- <v-cols cols="4" >
                                            
@@ -27,12 +22,12 @@
 
                                         <v-col cols="12" sm ="12" md="12">
                                             
-                                            <v-btn small>ประวัติการบริจาค</v-btn>
+                                            <v-btn small style="width:auto;">ประวัติการบริจาค</v-btn>
                                         </v-col>
 
                                         <v-col cols="12" sm ="12" md="12">
                                             
-                                            <v-btn small  >ประวัติการซื้อ coin</v-btn>
+                                            <v-btn small style="width:auto;" >ประวัติการซื้อ coin</v-btn>
                                         </v-col>
 
                                         <!-- <v-cols cols="4" >
@@ -46,32 +41,24 @@
                         </div>
                         <div class="col-sm-8">
                             <div style="text-align:right; margin-right:3%;"><router-link style="color:gray; " to="/editprofile"><v-btn color="secondary" icon ><v-icon>mdi-pencil</v-icon></v-btn></router-link></div>
-                            <div class="card-block">       
-                                <!-- <div style="text-align: right;"><router-link style="color:gray; " to="/editprofile"><i class="fa fa-edit"></i>Edit</router-link></div> -->
-                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600" style="font-size:20px;">Profile</h6>
+                            <div class="card-block">
+                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Profile</h6>
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <p class="m-b-10 f-w-600">Email</p>
-                                        <v-text-field value="" v-bind:label="dataUser.email"  solo readonly  ></v-text-field>
-                                        <!-- <h6 style="width:100px" class="text-muted f-w-400">{{dataUser.email}}</h6> -->
+                                        <h6 class="text-muted f-w-400">{{dataUser.email}}</h6>
                                     </div>
                                     <div class="col-sm-6">
                                         <p class="m-b-10 f-w-600">เบอร์โทรติดต่อ</p>
-                                        <v-text-field value="" v-bind:label="dataUser.phone"  solo readonly  >{{dataUser.phone}}</v-text-field>
-                                        <!-- <h6 class="text-muted f-w-400">{{dataUser.phone}}</h6> -->
+                                        <h6 class="text-muted f-w-400">{{dataUser.phone}}</h6>
                                     </div>
                                     <div class="col-sm-6">
                                         <p class="m-b-10 f-w-600">อายุ(ปี)</p>
-                                        <v-text-field value="" v-bind:label="dataUser.age"  solo readonly  ></v-text-field>
-                                        <!-- <h6 class="text-muted f-w-400">{{dataUser.age}}</h6> -->
+                                        <h6 class="text-muted f-w-400">{{dataUser.age}}</h6>
                                     </div>
-                                    <!-- <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">อายุ(ปี)</p>
-                                        <v-text-field value=""  solo readonly  ></v-text-field>
-                                        
-                                    </div> -->
                                 </div>
                                 
+                               
                             </div>
                         </div>
                     </div>
@@ -79,7 +66,8 @@
             </div>
         </div>
     </div>
-    </v-container>
+</div></center>
+
 </div>
 
 </template>
@@ -117,27 +105,25 @@ export default {
                     localStorage.removeItem('user_token')
 				}
 			}
-    },
-    methods: {
-		async sign_out () {
-			localStorage.removeItem('user_token')
-			await this.$router.push('/login')
-		}
-	},
+    }
 }
 </script>
-
-<style>
-       
-       body {
+<style scoped>
+body {
     background-color: #f9f9fa
+}
+img{
+    width: 200px;
+    height: 200px;
 }
 
 .padding {
     padding: 3rem !important
 }
 
+
 .user-card-full {
+    /* width: 700px; */
     overflow: hidden
 }
 
@@ -147,20 +133,18 @@ export default {
     box-shadow: 0 1px 20px 0 rgba(69, 90, 100, 0.08);
     border: none;
     margin-bottom: 30px
-    
 }
 
 .m-r-0 {
-    margin-right: 0px;
-    
+    margin-right: 0px
 }
 
 .m-l-0 {
-    margin-left: 0px;
-    
+    margin-left: 0px
 }
 
 .user-card-full .user-profile {
+    
     border-radius: 5px 0 0 5px
 }
 
@@ -170,10 +154,12 @@ export default {
 }
 
 .user-profile {
+    
     padding: 20px 0
 }
 
 .card-block {
+    
     padding: 1.25rem
 }
 
@@ -193,23 +179,28 @@ h6 {
     line-height: 25px
 }
 
-
 @media only screen and (min-width: 1400px) {
     p {
         font-size: 14px
     }
-    
 }
-@media only screen and (max-width: 820px) {
-    p {
-        font-size: 14px
-    }
-    #borderbox1{
-        width:300px;
-    }
-    #borderbox2{
-        width:600px;
-    }
+@media only screen and (max-width: 991px) {
+   img{
+    width: 150px;
+    height: 150px;
+}
+}
+@media only screen and (max-width: 770px) {
+   img{
+    width: 100px;
+    height: 100px;
+}
+}
+@media only screen and (max-width: 576px) {
+   img{
+    width: 200px;
+    height: 200px;
+}
 }
 
 
@@ -238,7 +229,7 @@ h6 {
 }
 
 .text-muted {
-    color: #919aa3 
+    color: #919aa3 !important
 }
 
 .b-b-default {
@@ -267,7 +258,6 @@ h6 {
 
 .m-t-40 {
     margin-top: 20px
-     
 }
 
 .user-card-full .social-link li {
@@ -279,33 +269,5 @@ h6 {
     margin: 0 10px 0 0;
     -webkit-transition: all 0.3s ease-in-out;
     transition: all 0.3s ease-in-out
-}
-
-/* previewsimage */
-
- .base-image-input {
-  display: block;
-  width: 200px;
-  height: 200px;
-  
-  background-size: center;
-  background-position: center center;
-}
-.placeholder {
-  background: #F0F0F0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #333;
-  font-size: 8px;
-  font-family: Helvetica;
-}
-.placeholder:hover {
-  background: #E0E0E0;
-}
-.file-input {
-  display: none;
 }
 </style>
