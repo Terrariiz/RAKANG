405015a5f8a5258b06aaaab0c47c30e64ca268b7
pitpagon <pitpagon@hotmail.com>
2021-02-12 21:58:28 +0700

    log donate table

--- client/src/components/admin/LogDonate.vue
+++ client/src/components/admin/LogDonate.vue
@@ -3,9 +3,24 @@
     <div>
       <Navbar></Navbar>
     </div>
-    <br>
+    <h1>Log Donate Admin</h1>
     <v-container >
-      <h1>Log Donate Admin</h1>
+      <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+    ></v-data-table>
+  </v-card>
     </v-container>    
   </div>
 </template>
@@ -16,6 +31,21 @@ export default {
   name: 'Admin',
   components:{
     Navbar
+  },
+  data () {
+      return {
+        search: '',
+        headers: [
+          {text: 'ชื่อแคมเปญ',value: 'Campaign',},
+          { text: 'จำนวนเงินที่บริจาค(บาท)', value: 'amonut' },
+          { text: 'วัน-เดือน-ปี', value: 'date' },
+          
+        ],
+      }
   }
+      
 }
+
+
+
 </script>
