317d65662537667ee28440990ed06995ebc80bca
pitpagon <pitpagon@hotmail.com>
2021-02-15 17:06:11 +0700

    home card ...

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -54,32 +54,38 @@
       </v-container>
     
 
-    <!-- ข่าวล่าสุด -->
+     <!-- ข่าวล่าสุด -->
     <div class="block latestPostBlock">
       <v-container>
         <h2 class="text-center">ข่าว</h2>
         <v-row>
           <v-col v-for="news in news" :key="news.id" cols="12" md="4">
-            <v-card  outlined class="mx-auto">
-              <v-img
-                class="white--text align-end"
-                height="200px"
-                :src="news.src"
-              >
-                <v-card-title>{{ news.name }}</v-card-title>
-              </v-img>
-              <v-card-subtitle class="pb-0">{{
-                news.subtitle
-              }}</v-card-subtitle>
-              <v-card-text class="text--primary">
-                <div>{{ news.description }}</div>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="primary" style="margin-left:auto;" text
+            <v-card
+      class="mx-auto"
+      max-width="344"
+      outlined
+    >
+     <v-img :src="news.src"></v-img>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-4">
+           {{ news.name }}
+          </div>
+          <v-list-item-title class="headline mb-1">
+            {{news.subtitle}}
+          </v-list-item-title>
+          <v-list-item-subtitle>{{ news.description }}</v-list-item-subtitle>
+        </v-list-item-content>
+  
+      
+      </v-list-item>
+  
+      <v-card-actions>
+                <v-btn to="/test" color="primary" style="margin-left:auto;" text
                   >More</v-btn
                 >
               </v-card-actions>
-            </v-card>
+    </v-card>
           </v-col>
         </v-row>
         <a href="/home" style="float:right; margin-bottom:3%"
@@ -88,6 +94,9 @@
         ></a>
       </v-container>
     </div>
+
+
+    
   </div>
 </template>
 
@@ -118,7 +127,7 @@ export default {
           subtitle: "1,000 miles of wonder",
           description:
             "His ubique laboramus ne. Expetenda assueverit sed ad. Id nec malis lucilius delicatissimi. Nec assum sonet suscipit ex, diam deterruisset ut usu, ad dicat fabellas aliquando eam.",
-          // src: require("../assets/images/tenor.gif"),
+          src: require("../assets/images/tenor.gif"),
         },
         {
           id: 2,
@@ -127,7 +136,7 @@ export default {
           subtitle: "2,000 miles of wonder",
           description:
             "Sea ad habemus assueverit, omnes platonem convenire sit et, at integre pericula quo. Facete adolescens definitionem cu qui, in putant aliquid fierent ius.",
-          // src: require("../assets/images/tenor.gif"),
+          src: require("../assets/images/tenor.gif"),
         },
         {
           id: 3,
@@ -136,7 +145,7 @@ export default {
           subtitle: "3,000 miles of wonder",
           description:
             "Aliquam albucius mei ei, debitis torquatos et pro, eos natum scribentur no. Putant verear constituto te qui. Adolescens persequeris vim ei. Vel nullam reprimique te.",
-          // src: require("../assets/images/tenor.gif"),
+          src: require("../assets/images/tenor.gif"),
         },
       ],
     };
