fb503df7b7dbf788c29e8ba33bbc74f9ebb9079e
pitpagon <pitpagon@hotmail.com>
2021-02-17 22:49:05 +0700

    new page แคมเปญ

--- client/src/components/admin/AddCampaign.vue
+++ client/src/components/admin/AddCampaign.vue
@@ -3,7 +3,7 @@
         <div>
             <Navbar></Navbar>
         </div>
-         <form
+         <!-- <form
          @submit.prevent="Addcampaign">
         <v-container id ='rounded' style="background-color: #F09C0B;">
             <center><h1>Add new Campaign</h1></center>
@@ -47,7 +47,113 @@
                 </div>
 
         </v-container>
-        </form>    
+        </form> -->
+
+
+        <!-- อันใหม่ยังไม่ใช่ form     -->
+        <form
+         @submit.prevent="Addcampaign">
+         <div class="project-header">
+    <v-container >
+      <v-row >
+               <h1>Add new campaign</h1>
+              </v-row>
+      <v-row class="headname" >
+          <v-text-field v-model="campaign.name"  solo label="Name"  required></v-text-field>
+          <br>
+      </v-row>
+      <v-row>
+          <v-col  cols="12" md="8" sm="12">
+              <div class="project-content">
+                  <!-- <img class="image -fullwidth img-responsive" id="showimage" :src="'http://localhost:4000/uploads/' + campaign.image"/> -->
+                   <v-container id = "picturenews">
+                                <center><v-div required style=""  class="base-image-input" :style="{ 'background-image': `url(${imageData})` }" @click="chooseImage">
+                                    <span  v-if="!imageData"  class="placeholder">Choose an Image</span>
+                                    <input  class="file-input" id="file-input"  ref="fileInput"  type="file"  v-on:change="onFileSelected" >
+                                </v-div></center>
+
+                                <hr>
+                            </v-container>
+                  <v-textarea solo
+                    clearable
+                    v-model="campaign.content"
+                    clear-icon="mdi-close-circle"
+                    label="รายละเอียดของแคมเปญ"
+                    value="" required
+                  ></v-textarea>
+                  <!-- <p class="lead">content</p> -->
+                  <p class="details">
+                      <v-text-field v-model="campaign.date" type="date" solo label="วันสิ้นสุดแคมเปญ" required></v-text-field>
+                      <v-text-field class="location"  solo label="สถานที่" required></v-text-field>
+                      <!-- <span class="location">สถานที่</span> -->
+                  </p>
+              </div>
+          </v-col>
+          <v-col  cols="12" md="4" sm="12">
+            <div class="fund-raising affix-top">
+                <div class="body">
+                    <!-- <div class="amount-raised">
+                        <h3 class="title">ยอดบริจาคขณะนี้</h3>
+                        <span class="value">100000 บาท</span>
+                    </div> -->
+                    <div class="funding-goal">
+                        <h3 class="title">เป้าหมาย</h3>
+                         <v-text-field v-model="campaign.amount" class="value" solo label="จำนวนเงิน" required ></v-text-field>
+                        
+                    </div>
+                    <!-- <div class="progress-bar">
+                        <span class="percent">
+                            <span class="hide-txt">ดำเนินการไปแล้ว</span>
+                            80%
+                        </span>
+                        <span class="bar" style="width:80%"></span>
+                    </div> -->
+                    <!-- <span class="timeleft">365 วัน</span>
+                    <span class="people">
+                        <span class="hide-txt">จำนวนคนที่บริจาค</span>
+                        <span class="icon-people">43</span>
+                    </span> -->
+                    
+                </div>
+                    
+            <!-- <div class="action">
+                <a><v-btn block> </v-btn></a>
+                
+            </div> -->
+
+            </div>
+          </v-col>
+      </v-row>
+      <v-row>
+         <v-col  cols="12" md="8" sm="12">
+           <h3>ภาพรวม</h3>
+           <br>
+            <ckeditor 
+              id="content"
+              v-model="content"
+              @input="onEditorInput" required> 
+            </ckeditor>
+            <hr>
+            <h3>ความคืบหน้า</h3>
+            <br>
+              <ckeditor 
+                id="content"
+                v-model="content"
+                @input="onEditorInput" required>
+            </ckeditor>
+         </v-col>
+      </v-row>
+      
+  </v-container>
+   <div id="grid-container">
+                    <div></div>
+                    <v-btn style="weidth = 40%" color="error" dark href='/admin/listnews'>cancle</v-btn>
+                    <v-btn type="submit" color="primary" dark>submit</v-btn>
+                    <div></div>  
+                </div>
+    </div>
+        </form>
+  
     </div>
 </template>
 
@@ -102,6 +208,122 @@
 .file-input {
   display: none;
 }
+
+/* ฟอมอันใหม่ */
+.project-header{
+  background-color: #fff8ec;
+}
+.project-content{
+    padding-right: 40px;
+    
+}
+.image .-fullwidth{
+  width: 100%;
+}
+.image{
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 40px;
+}
+.img-responsive{
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+img{
+  vertical-align: middle;
+  border: 0;
+}
+.fund-raising{
+    border-radius: 6px;
+    background-clip: padding-box;
+    color: #666;
+    border: 1px solid #f3d9ab;
+    background-color: #fff;
+    z-index: 999;
+}
+.fund-raising .body{
+   padding: 30px 25px 0  25px;
+}
+.fund-raising .action{
+   padding: 30px 25px 30px 25px;
+}
+
+#picturenews{
+    /* margin-top: 10%; */
+    /* margin-bottom: 10%; */
+    height:70% ;
+    width: 70% ;
+}
+
+/* previewsimage */
+
+ .base-image-input {
+  display: block;
+  width: 300px;
+  height: 300px;
+  cursor: pointer;
+  background-size: cover;
+  background-position: center center;
+}
+.placeholder {
+  background: #F0F0F0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #333;
+  font-size: 20px;
+  font-family: Helvetica;
+}
+.placeholder:hover {
+  background: #E0E0E0;
+}
+.file-input {
+  display: none;
+}
+#grid-container{
+    display: grid;
+    /* background-color: white; */
+    padding: 10px;
+    grid-template-columns: auto 10% 10% auto;
+    grid-column-gap: 10%;
+}
+.headname{
+  width:30%;
+ 
+}
+.details{
+  width:40%;
+}
+@media(max-width: 767px){
+    .project-content {
+    padding-right: 0;
+  }
+ 
+}
+@media(max-width: 321px){
+    
+ .base-image-input {
+  
+  width: 100px;
+  height: 100px;
+  }
+  .placeholder{
+    font-size: 10px;
+  }
+  .headname{
+  width:100%;
+  margin-left: auto;
+  margin-right: auto;
+}
+.details{
+  width:100%;
+}
+}
+
+
 </style>
 
 <script>
@@ -113,6 +335,11 @@ export default {
   name: 'Addcampaign',
   data(){
         return{
+          tab: null,
+        items: [
+          'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',        
            imageData:null,
            campaign: {
                 name: null,
@@ -120,7 +347,13 @@ export default {
                 image: null,
                 imagepath: "" ,
                 date: null,
-                amount: null
+                amount: null,
+                tab: null,
+        items: [
+          'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        
             },
         }
     },
--- client/src/components/admin/test.vue
+++ client/src/components/admin/test.vue
@@ -1,137 +1,261 @@
-<template class="body">
-  <div  >
-    <!-- ข่าวล่าสุด -->
-    <div class="block latestPostBlock">
-      <v-container>
-        <h2 class="text-center">ข่าว</h2>
-        <v-row>
-          <v-col v-for="news in news" :key="news.id" cols="12" md="4">
-            <v-card
-      class="mx-auto"
-      max-width="344"
-      outlined
-    >
-     <v-img :src="news.src"></v-img>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">
-           {{ news.name }}
-          </div>
-          <v-list-item-title class="headline mb-1">
-            {{news.subtitle}}
-          </v-list-item-title>
-          <v-list-item-subtitle>{{ news.description }}</v-list-item-subtitle>
-        </v-list-item-content>
+<template>
+<div>
   
-      
-      </v-list-item>
-  
-      <v-card-actions>
-                <v-btn color="primary" style="margin-left:auto;" text
-                  >More</v-btn
-                >
-              </v-card-actions>
-    </v-card>
+    <div class="project-header">
+    <v-container >
+      <v-row >
+               <h1>Add new campaign</h1>
+              </v-row>
+      <v-row class="headname" >
+          <v-text-field  solo label="Name"  ></v-text-field>
+          <br>
+      </v-row>
+      <v-row>
+          <v-col  cols="12" md="8" sm="12">
+              <div class="project-content">
+                  <!-- <img class="image -fullwidth img-responsive" id="showimage" :src="'http://localhost:4000/uploads/' + campaign.image"/> -->
+                   <v-container id = "picturenews">
+                                <center><v-div style=""  class="base-image-input" :style="{ 'background-image': `url(${imageData})` }" @click="chooseImage">
+                                    <span  v-if="!imageData"  class="placeholder">Choose an Image</span>
+                                    <input  class="file-input" id="file-input"  ref="fileInput"  type="file"  v-on:change="onFileSelected" >
+                                </v-div></center>
+
+                                <hr>
+                            </v-container>
+                  <v-textarea solo
+                    clearable
+                    clear-icon="mdi-close-circle"
+                    label="รายละเอียดของแคมเปญ"
+                    value=""
+                  ></v-textarea>
+                  <!-- <p class="lead">content</p> -->
+                  <p class="details">
+                      <v-text-field  type="date" solo label="วันสิ้นสุดแคมเปญ" required></v-text-field>
+                      <v-text-field class="location"  solo label="สถานที่" required></v-text-field>
+                      <!-- <span class="location">สถานที่</span> -->
+                  </p>
+              </div>
           </v-col>
-        </v-row>
-        <a href="/home" style="float:right; margin-bottom:3%"
-          >แสดงทั้งหมด <i class="fa fa-chevron-right" aria-hidden="true"></i
-          ><i class="fa fa-chevron-right" aria-hidden="true"></i
-        ></a>
-      </v-container>
-    </div>
+          <v-col  cols="12" md="4" sm="12">
+            <div class="fund-raising affix-top">
+                <div class="body">
+                    <!-- <div class="amount-raised">
+                        <h3 class="title">ยอดบริจาคขณะนี้</h3>
+                        <span class="value">100000 บาท</span>
+                    </div> -->
+                    <div class="funding-goal">
+                        <h3 class="title">เป้าหมาย</h3>
+                         <v-text-field class="value" solo label="จำนวนเงิน" ></v-text-field>
+                        
+                    </div>
+                    <!-- <div class="progress-bar">
+                        <span class="percent">
+                            <span class="hide-txt">ดำเนินการไปแล้ว</span>
+                            80%
+                        </span>
+                        <span class="bar" style="width:80%"></span>
+                    </div> -->
+                    <!-- <span class="timeleft">365 วัน</span>
+                    <span class="people">
+                        <span class="hide-txt">จำนวนคนที่บริจาค</span>
+                        <span class="icon-people">43</span>
+                    </span> -->
+                    
+                </div>
+                    
+            <!-- <div class="action">
+                <a><v-btn block> </v-btn></a>
+                
+            </div> -->
 
-    <!-- text -->
-     <!-- <v-card
-      class="mx-auto"
-      max-width="344"
-      outlined
-    >
-     <v-img src="../../../public/image/2.jpg"></v-img>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">
-            OVERLINE
-          </div>
-          <v-list-item-title class="headline mb-1">
-            Headline 5
-          </v-list-item-title>
-          <v-list-item-subtitle>Greyjgkgkkjhkhkhkjhound divisely heกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกllo coldly fonwderfully</v-list-item-subtitle>
-        </v-list-item-content>
-  
+            </div>
+          </v-col>
+      </v-row>
+      <v-row>
+         <v-col  cols="12" md="8" sm="12">
+           <h3>ภาพรวม</h3>
+           <br>
+            <ckeditor 
+              id="content"
+              v-model="content"
+              @input="onEditorInput">
+            </ckeditor>
+            <hr>
+            <h3>ความคืบหน้า</h3>
+            <br>
+              <ckeditor 
+                id="content"
+                v-model="content"
+                @input="onEditorInput">
+            </ckeditor>
+         </v-col>
+      </v-row>
       
-      </v-list-item>
-  
-      <v-card-actions>
-                <v-btn color="primary" style="margin-left:auto;" text
-                  >More</v-btn
-                >
-              </v-card-actions>
-    </v-card> -->
-
-  </div>
+  </v-container>
+   <div id="grid-container">
+                    <div></div>
+                    <v-btn style="weidth = 40%" color="primary" dark href='/admin/listnews'>cancle</v-btn>
+                    <v-btn type="submit" color="primary" dark>submit</v-btn>
+                    <div></div>  
+                </div>
+    </div>
+    
+ 
+</div>
 </template>
-
 <script>
 
-//  import x from "../../../public/image/2.jpg"
-
-
+import moment from "moment";
 export default {
-  name: "Home",
-  data() {
-    return {
-      // items: [
-      //   {
-      //     src: require("../assets/images/tenor2.gif"),
-      //   },
-      //   {
-      //     src: require("../assets/images/tenor3.gif"),
-      //   },
-      //   {
-      //     src: require("../assets/images/tenor4.gif"),
-      //   },
-      // ],
-
-      show: false,
-      news: [
-        {
-          id: 1,
-          name: "No.1",
-          title: "Top western road trips",
-          subtitle: "1,000 miles of wonder",
-          description:
-            "His ubique laboramus ne. Expetenda assueverit sed ad. Id nec malis lucilius delicatissimi. Nec assum sonet suscipit ex, diam deterruisset ut usu, ad dicat fabellas aliquando eam.",
-          // src: require("../assets/images/tenor.gif"),
+    name:'Campaign',
+    components:{
+        
+    },
+    data (){
+      return {
+        campaign: null,
+        tab: null,
+        items: [
+          'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        }
+    },
+    mounted: function(){
+      this.getData()
+    },
+    
+    methods: {
+        getData(){
+            var that = this;
+            this.$http.get("/campaign/DetailCampaign/"+this.$route.params.id)
+            .then((res) => {
+              console.log(res.data)
+              that.campaign = res.data;
+              console.log(that.campaign)
+              that.campaign.date = moment(that.campaign.date).format(" dddd DD-MM-YY  A");
+            })
+            .catch(function(err){
+              console.log(err)
+            })
         },
-        {
-          id: 2,
-          name: "No.2",
-          title: "Christmas tales to read",
-          subtitle: "2,000 miles of wonder",
-          description:
-            "Sea ad habemus assueverit, omnes platonem convenire sit et, at integre pericula quo. Facete adolescens definitionem cu qui, in putant aliquid fierent ius.",
-          // src: require("../assets/images/tenor.gif"),
-        },
-        {
-          id: 3,
-          name: "No.3",
-          title: "20 movies not to miss in 2020",
-          subtitle: "3,000 miles of wonder",
-          description:
-            "Aliquam albucius mei ei, debitis torquatos et pro, eos natum scribentur no. Putant verear constituto te qui. Adolescens persequeris vim ei. Vel nullam reprimique te.",
-          // src: require("../assets/images/tenor.gif"),
-        },
-      ],
-    };
-  },
-  components: {
-   
-  },
-};
-  
+    }
+}
 </script>
 
 <style>
+.project-header{
+  background-color: #fff8ec;
+}
+.project-content{
+    padding-right: 40px;
+    
+}
+.image .-fullwidth{
+  width: 100%;
+}
+.image{
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 40px;
+}
+.img-responsive{
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+img{
+  vertical-align: middle;
+  border: 0;
+}
+.fund-raising{
+    border-radius: 6px;
+    background-clip: padding-box;
+    color: #666;
+    border: 1px solid #f3d9ab;
+    background-color: #fff;
+    z-index: 999;
+}
+.fund-raising .body{
+   padding: 30px 25px 0  25px;
+}
+.fund-raising .action{
+   padding: 30px 25px 30px 25px;
+}
+
+#picturenews{
+    /* margin-top: 10%; */
+    /* margin-bottom: 10%; */
+    height:70% ;
+    width: 70% ;
+}
+
+/* previewsimage */
+
+ .base-image-input {
+  display: block;
+  width: 300px;
+  height: 300px;
+  cursor: pointer;
+  background-size: cover;
+  background-position: center center;
+}
+.placeholder {
+  background: #F0F0F0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #333;
+  font-size: 20px;
+  font-family: Helvetica;
+}
+.placeholder:hover {
+  background: #E0E0E0;
+}
+.file-input {
+  display: none;
+}
+#grid-container{
+    display: grid;
+    /* background-color: white; */
+    padding: 10px;
+    grid-template-columns: auto 10% 10% auto;
+    grid-column-gap: 10%;
+}
+.headname{
+  width:30%;
+ 
+}
+.details{
+  width:40%;
+}
+@media(max-width: 767px){
+    .project-content {
+    padding-right: 0;
+  }
+ 
+}
+@media(max-width: 321px){
+    
+ .base-image-input {
+  
+  width: 100px;
+  height: 100px;
+  }
+  .placeholder{
+    font-size: 10px;
+  }
+  .headname{
+  width:100%;
+  margin-left: auto;
+  margin-right: auto;
+}
+.details{
+  width:100%;
+}
+}
+
 
 </style>
