2be8c705a5932dd85136b3b2345642e8e6cec696
TRP <akemonai@gmail.com>
2021-03-24 17:06:14 +0700

    nav test

---
+++ client/src/components/admin/test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Navbar
+      name="Logo Name"
+      :navLinks="[
+        {
+          name: 'Home',
+          link: '/home',
+          dropdown: false,
+        },
+{ name: 'About', link: '/about', dropdown: false },
+{
+          name: 'Contacts',
+          link: '/contact',
+          dropdown: true,
+          dropdownLinks: [ 
+            { name: 'A', link: '/pA', dropdown: false },
+            { name: 'B', link: '/pB', dropdown: false },
+          ],
+        },
+        {
+          name: 'Projects',
+          link: '/projects',
+          dropdown: true,
+          dropdownLinks: [ 
+            { name: 'C', link: '/pA', dropdown: false },
+            { name: 'D', link: '/pB', dropdown: false },
+          ],
+        },
+      ]"
+    />
+  </div>
+</template>
+<script>
+const Navbar = () => import('@/components/navbar/admin_navbar')
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  },
+}
+</script>
