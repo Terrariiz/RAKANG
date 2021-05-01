6080186741ece2afeb8c7f13dffad69fbc7a63a6
TRP <akemonai@gmail.com>
2021-05-02 03:56:41 +0700

    contact

--- client/src/components/navbar/footer.vue
+++ client/src/components/navbar/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    v-if="!($store.getters.AdminIsLoggedIn)"
+    v-if="!$store.getters.AdminIsLoggedIn"
     style=" bottom: 0;
             width: 100%;
             /* Height of the footer*/ 
@@ -11,13 +11,14 @@
         <v-btn text rounded class="my-2" to="/termservice">
           ข้อตกลงการใช้งานเว็บไซต์
         </v-btn>
-        <v-btn  text rounded class="my-2" to="/termuse">
+        <v-btn text rounded class="my-2" to="/termuse">
           วิธีการใช้งานเว็บไซต์
         </v-btn>
-        <v-col class="py-4 text-center white--text" color="#CFD6EA" cols="12">
-          {{ new Date().getFullYear() }} — <strong>RAKANGTHAM</strong>
-        </v-col>
       </v-row>
+        <v-col class="py-4 text-center white--text" color="#CFD6EA" cols="12">
+          {{ new Date().getFullYear() }} — <router-link to='/contact'> <strong>RAKANGTHAM</strong></router-link>
+        </v-col>
+      
     </v-footer>
   </div>
 </template>
@@ -34,4 +35,6 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+
+</style>
