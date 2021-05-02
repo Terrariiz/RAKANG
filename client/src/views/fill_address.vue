ca692b50eab4dec04beab187e8a5b1c44562a5e3
kluaynum1150 <kluaynum951@gmail.com>
2021-05-03 06:16:48 +0700

    หน้ากรอกที่อยู่ 100%

--- client/src/views/fill_address.vue
+++ client/src/views/fill_address.vue
@@ -11,6 +11,7 @@
           color="green"
         ></v-progress-circular>
       </v-overlay>
+      <v-btn style="margin-left: 5%;" @click="$router.back()"><i style="float:left;" class="fa fa-arrow-left fa-lg" aria-hidden="true"></i></v-btn>
     <div class="head-fill"><h1>กรอกที่อยู่ในการจัดส่ง</h1></div>
     <div v-if="addressUser == null" class="container">
       <h1 class="head">ที่อยู่จัดส่ง</h1>
@@ -99,7 +100,7 @@
       <v-radio
         color="success"
         label="ใช้ที่อยู่ปัจจุบัน"
-        :value="true"
+        :value="false"
       ></v-radio>
       <!-- <v-checkbox
               v-model="ex4"
@@ -115,6 +116,7 @@
                 :style="{
                     'border': hasFocus && currentColor !== '#f5f5f5' ? 'solid 1px ' + currentColor : 'solid 1px #d3d3d3'
                 }"
+                disabled
                 v-model="addressUser.name"
                 name="name"
                 type="text"
@@ -129,45 +131,49 @@
                 }"
                 name="phone"
                 type="text"
+                disabled
                 v-model="addressUser.phone"
                 required
             >
           </v-col>
-
-          <v-col cols="12" md="4" sm="6">
-              <ThailandAutoComplete
-            required
-            v-model="addressUser.Sub_District"
-            type="district"
-            @select="select2"
-            label="แขวง/ตำบล"
-            color="#42b883"
-            size="default"
-            placeholder="แขวง/ตำบล..."/>
-          </v-col>
-          <v-col cols="12" md="4" sm="6">
-            <ThailandAutoComplete required v-model="addressUser.District" type="amphoe" @select="select2" label="เขต/อำเภอ" placeholder="เขต/อำเภอ..."/>
-          </v-col>
-          <v-col cols="12" md="4" sm="6">
-            <ThailandAutoComplete required v-model="addressUser.province" type="province" @select="select2" label="จังหวัด" color="#35495e" placeholder="จังหวัด..."/>
-          </v-col>
-          <v-col cols="12" md="4" sm="6">
-            <ThailandAutoComplete required v-model="addressUser.postcode" type="zipcode" @select="select2" label="รหัสไปรษณีย์" color="#00a4e4" placeholder="รหัสไปรษณีย์..."/>
-          </v-col>
-          <v-col cols="12" md="8" sm="12">
+          <v-col cols="12" md="12" sm="12">
             <label for="phone">ที่อยู่</label>
             <v-textarea
               clearable
               v-model="addressUser.locationdetail"
               solo
+              disabled
               filled
               auto-grow
               clear-icon="mdi-close-circle"
               label="46 หมู่ 4 ถนน......."
             ></v-textarea>
           </v-col>
+          <v-col cols="12" md="4" sm="6">
+            <label>แขวง/ตำบล</label>
+              <input
+            required
+            v-model="addressUser.Sub_District"
+            :style="{ 'border': hasFocus && currentColor !== '#f5f5f5' ? 'solid 1px ' + currentColor : 'solid 1px #d3d3d3'}"
+            disabled
+            type="text"
+            
+            placeholder="แขวง/ตำบล..."/>
+          </v-col>
+          <v-col cols="12" md="4" sm="6">
+            <label>เขต/อำเภอ</label>
+            <input :style="{ 'border': hasFocus && currentColor !== '#f5f5f5' ? 'solid 1px ' + currentColor : 'solid 1px #d3d3d3'}" required disabled v-model="addressUser.District" type="text" placeholder="เขต/อำเภอ..."/>
+          </v-col>
+          <v-col cols="12" md="4" sm="6">
+            <label>จังหวัด</label>
+            <input :style="{ 'border': hasFocus && currentColor !== '#f5f5f5' ? 'solid 1px ' + currentColor : 'solid 1px #d3d3d3'}" required disabled v-model="addressUser.province" type="text" placeholder="จังหวัด..."/>
+          </v-col>
+          <v-col cols="12" md="4" sm="6">
+            <label>รหัสไปรษณีย์</label>
+            <input :style="{ 'border': hasFocus && currentColor !== '#f5f5f5' ? 'solid 1px ' + currentColor : 'solid 1px #d3d3d3'}" required disabled v-model="addressUser.postcode" type="text" placeholder="รหัสไปรษณีย์..."/>
+          </v-col>
         </v-row>
-         <div class="btn"><v-btn color="success" @click="confirmAddress" type="submit">ยืนยันสินค้า</v-btn></div>
+         <div class="btn"><v-btn v-if="!selectAddress" color="success" @click="confirmAddress" type="submit">ยืนยันสินค้า</v-btn></div>
     </div>
 
     <div v-if="addressUser != null" class="container">
@@ -176,7 +182,7 @@
       <v-radio
         color="success"
         label="ใช้ที่อยู่ใหม่"
-        :value="false"
+        :value="true"
       ></v-radio>
       <!-- <v-checkbox
               v-model="ex4"
@@ -245,7 +251,7 @@
           </v-col>
           
         </v-row>
-         <div class="btn"><v-btn color="success" @click="confirmAddress" type="submit">ยืนยันสินค้า</v-btn></div>
+         <div class="btn"><v-btn v-if="selectAddress" color="success" @click="confirmAddress" type="submit">ยืนยันสินค้า</v-btn></div>
     </div>
     </v-radio-group>
     <br />
@@ -263,7 +269,7 @@ import swal from "sweetalert2";
 export default {
     components: {
         Navbar,
-        Footer,
+        Footer
     },
     data () {
         return {
@@ -277,7 +283,7 @@ export default {
               province: '',
               zipcode: ''
             },
-            selectAddress: true,
+            selectAddress: null,
             isloading: false,
         }
     },
@@ -296,31 +302,22 @@ export default {
     },
     async confirmAddress(){
       try {
-        if(this.newAddressUser.name == '' || this.newAddressUser.phone == '' || this.newAddressUser.district == '' || this.newAddressUser.amphoe == '' || this.newAddressUser.province == '' || this.newAddressUser.zipcode == '' || this.newAddressUser.address == ''){
-          swal.fire("เกิดข้อผิดพลาด", "กรุณากรอกข้อมูลให้ครบ", "error");
-        } else{
         this.isloading = true
-        const formData = new FormData();
+        const formData = new URLSearchParams()
         if(this.selectAddress == true){
-          formData.append('name', this.newAddressUser.name)
-          formData.append('phone', this.newAddressUser.phone)
-          formData.append('locationdetail', this.newAddressUser.address)
-          formData.append('Sub_District', this.newAddressUser.district)
-          formData.append('District', this.newAddressUser.amphoe)
-          formData.append('province', this.newAddressUser.province)
-          formData.append('postcode', this.newAddressUser.zipcode)
-          formData.append('savelocation', this.selectAddress)
-        } else {
-          formData.append('name', this.addressUser.name)
-          formData.append('phone', this.addressUser.phone)
-          formData.append('locationdetail', this.addressUser.locationdetail)
-          formData.append('Sub_District', this.addressUser.Sub_District)
-          formData.append('District', this.addressUser.District)
-          formData.append('province', this.addressUser.province)
-          formData.append('postcode', this.addressUser.postcode)
-          formData.append('savelocation', this.selectAddress)
-        }
-        console.log(formData)
+          if(this.newAddressUser.name == '' || this.newAddressUser.phone == '' || this.newAddressUser.district == '' || this.newAddressUser.amphoe == '' || this.newAddressUser.province == '' || this.newAddressUser.zipcode == '' || this.newAddressUser.address == ''){
+            swal.fire("เกิดข้อผิดพลาด", "กรุณากรอกข้อมูลให้ครบ", "error");
+            this.isloading = false
+          } else{
+            formData.append('name', this.newAddressUser.name)
+            formData.append('phone', this.newAddressUser.phone)
+            formData.append('locationdetail', this.newAddressUser.address)
+            formData.append('Sub_District', this.newAddressUser.district)
+            formData.append('District', this.newAddressUser.amphoe)
+            formData.append('province', this.newAddressUser.province)
+            formData.append('postcode', this.newAddressUser.zipcode)
+            formData.append('savelocation', this.selectAddress)
+            console.log(formData)
         swal.fire({
                 title: 'คุณแน่ใจแล้วใช่ไหมว่าที่อยู่จัดส่งถูกต้อง',
                 icon: 'question',
@@ -343,8 +340,48 @@ export default {
                         swal.fire("Error", "Something Went Wrong", "error");
                         console.log('error')
                     }
-                }
+                } else { this.isloading = false }
             })
+          }
+        } else{
+          if(this.addressUser.name == '' || this.addressUser.phone == '' || this.addressUser.locationdetail == '' || this.addressUser.Sub_District == '' || this.addressUser.District == '' || this.addressUser.province == '' || this.addressUser.postcode == ''){
+            swal.fire("เกิดข้อผิดพลาด", "กรุณากรอกข้อมูลให้ครบ", "error");
+            this.isloading = false
+          } else{
+            formData.append('name', this.addressUser.name)
+            formData.append('phone', this.addressUser.phone)
+            formData.append('locationdetail', this.addressUser.locationdetail)
+            formData.append('Sub_District', this.addressUser.Sub_District)
+            formData.append('District', this.addressUser.District)
+            formData.append('province', this.addressUser.province)
+            formData.append('postcode', this.addressUser.postcode)
+            formData.append('savelocation', this.selectAddress)
+            console.log(formData)
+        swal.fire({
+                title: 'คุณแน่ใจแล้วใช่ไหมว่าที่อยู่จัดส่งถูกต้อง',
+                icon: 'question',
+                confirmButtonColor: 'green',
+                cancelButtonColor: 'red',
+                showCancelButton: true,
+                confirmButtonText: `ใช่`,
+                cancelButtonText: `ไม่ใช่`,
+            }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                    let suscess = this.$http.post("/exchangeitem/DetailItem/"+this.$route.params.id+"/Perchase/"+this.$route.params.userID, formData);
+                    console.log(suscess);
+                    if (suscess) {
+                        this.$router.push({ name: 'Home'})
+                        swal.fire("สำเร็จ", "แลกของรางวัลเรียบร้อยแล้ว", "success");
+                        console.log('success')
+                    } else {
+                        this.isloading = false
+                        swal.fire("Error", "Something Went Wrong", "error");
+                        console.log('error')
+                    }
+                } else { this.isloading = false }
+            })
+          }
         }
         } catch (err) {
           this.isloading = false
@@ -357,7 +394,7 @@ export default {
               console.log('error')
             }
         }
-    },
+    }
   },
   mounted: async function mounted() {
     await this.$http
@@ -365,9 +402,11 @@ export default {
       .then((res) => {
         console.log(res.data)
         console.log(res.data.userlocation)
-        if(res.data.userlocation == undefined){
+        if(res.data.userlocation.name == ''){
+          this.selectAddress = true
           this.addressUser = null
         } else {
+          this.selectAddress = false
           this.addressUser = res.data.userlocation
         }
       })
